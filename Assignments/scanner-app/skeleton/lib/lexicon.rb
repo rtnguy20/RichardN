11242f13f726718a770a71796e44a58bac5fcdc4
Richard N <rickky8555@hotmail.com>
2019-10-01 16:53:49 +0000

    More exercises attempted

--- Assignments/scanner-app/skeleton/lib/lexicon.rb
+++ Assignments/scanner-app/skeleton/lib/lexicon.rb
@@ -1,39 +1,34 @@
 class Lexicon
     attr_accessor :lexicon, :pair
     
-    stuff = gets.chomp()
-    words = stuff.split()    
-    
     Pair = Struct.new(:token, :word)
-    first_word = Pair.new("direction", "north")
-    second_word = Pair.new("direction", "south")
-    third_word = Pair.new("direction", "east")
-    fourth_word = Pair.new("direction", "north")
-    fifth_word = Pair.new("verb","go")
-    sixth_word = Pair.new("verb","kill")
-    seventh_word = Pair.new("verb","eat")
-    eighth_word = Pair.new("noun","bear")
-    ninth_word = Pair.new("noun","princess")
-    tenth_word = Pair.new("stop","the")
-    eleventh_word = Pair.new("stop","in")
-    twelfth_word = Pair.new("stop","of")
+    stuff = gets.chomp()
+    words = stuff.split() 
     
-    def scan(words)
-        pairs = [first_word, second_word, third_word, fourth_word, fifth_word, sixth_word, seventh_word, eighth_word, ninth_word, tenth_word, eleventh_word, twelfth_word]
-        words.map do |value|
-            if value.match(:word)
-                puts value
-            else 
-                pairs.push(Pair.new(:error,value.to_s))
-                
-            end
-            
-            begin 
-                Integer(words)
-            rescue ArgumentError
-                nil
-            end
-            
+    def scan(userwords)
+        sentence = []
+        userwords.each do { |word|
+        if word.include?("north","south", "east", "west")
+            sentence.push(Pair.new(:direction, word))
+        elsif word.include?("go", "stop","kill", "eat")
+            sentence.push(Pair.new(:verb, word))
+        elsif word.include?("door", "bear", "princess", "cabinet")
+            sentence.push(Pair.new(:noun,word))
+        elsif word.include?("the", "in", "of", "from", "at", "it")
+            sentence.push(Pair.new(:stop, word))
+        elsif word.include?("0","1","2","3","4","5","6","7","8","9")
+            return :convert_numbers
+        else
+            sentence.push(Pair.new(:error,word))
+        end
+        }
     end
-end
+    
+    def convert_number(s)
+        begin
+            Integer(s)
+        rescue ArgumentError
+            nil
+        end
+    end
 end
